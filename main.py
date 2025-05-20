4a1bd5de78ac00d3a9b7c00a2e74e3efd6e94bcb
Lily Bevan <lilybevan31@gmail.com>
2025-05-20 22:05:54 +0100

    make it gooder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-import re
+import random as kitkats
+import re as cupcake_flavouring
 
 with open("sct_pre.txt", "r") as sct_pre:
     sct_pre_rainbow = sct_pre.read().splitlines()
@@ -8,23 +8,23 @@ with open("sct_post.txt", "w+") as sct_post:
     for line in sct_pre_rainbow:
         if "16040642" in line:
             print(line)
-            elephants = random.randint(0, 255) * 65536 + random.randint(0, 255) * 256 + random.randint(0, 255)
+            elephants = kitkats.randint(0, 255) * 65536 + kitkats.randint(0, 255) * 256 + kitkats.randint(0, 255)
             line = line.replace("16040642", str(elephants))
             print(line)
         sct_post.write(line + "\n")
 
 with open("sct_post.txt", "r") as sct_post_pre:
-    sct_post_pre_randomness = sct_post_pre.read().splitlines()
+    sct_post_pre_kitkatsness = sct_post_pre.read().splitlines()
 
 list_of_labels_so_good = []
-with open("sct_post.txt", "w+") as sct_post_pre_randomness_pre:
-    for line in sct_post_pre_randomness:
-        if re.compile(r'"([^"]+)" .+').search(line):
-            list_of_labels_so_good.append(re.search(r'"([^"]+)" .+', line).group(0).split('"')[1])
-    random.shuffle(list_of_labels_so_good)
+with open("sct_post.txt", "w+") as sct_post_pre_kitkatsness_pre:
+    for line in sct_post_pre_kitkatsness:
+        if cupcake_flavouring.compile(r'"([^"]+)" .+').search(line):
+            list_of_labels_so_good.append(cupcake_flavouring.search(r'"([^"]+)" .+', line).group(0).split('"')[1])
+    kitkats.shuffle(list_of_labels_so_good)
     i = -1
-    for line in sct_post_pre_randomness:
-        if re.compile(r'"([^"]+)" .+').search(line):
+    for line in sct_post_pre_kitkatsness:
+        if cupcake_flavouring.compile(r'"([^"]+)" .+').search(line):
             i += 1
-            line = line.replace((re.search(r'"([^"]+)" .+', line).group(0).split('"')[1]), list_of_labels_so_good[i])
-        sct_post_pre_randomness_pre.write(line + "\n")
+            line = line.replace((cupcake_flavouring.search(r'"([^"]+)" .+', line).group(0).split('"')[1]), list_of_labels_so_good[i])
+        sct_post_pre_kitkatsness_pre.write(line + "\n")
